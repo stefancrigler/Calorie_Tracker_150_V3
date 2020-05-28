e6e805eb8f5f081b4a6c99c268246622de9e44ae
stefancrigler <stefancrigler@ucsb.edu>
2020-05-27 19:14:30 -0700

    added new stuff for the leaderboards, now calorie goals are sorted in order

---
+++ app/src/main/java/com/example/calorie_king_stefan_rob/AddPreparationActivity.java
@@ -0,0 +1,15 @@
+package com.example.calorie_king_stefan_rob;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddPreparationActivity extends AppCompatActivity
+{
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+
+   }
+}
